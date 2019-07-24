5782ba10f638d16af7483d30e9285ef243569d36
Ananda Umamil <zweimach@galatulis.io>
2019-07-24 09:08:21 +0700

    fix: scriptsDir pointing to wrong path

--- scripts/utils/getDirPath.ts
+++ scripts/utils/getDirPath.ts
@@ -2,7 +2,7 @@ import { resolve, join } from "path";
 
 export const rootDir = resolve(__dirname, "..", "..");
 
-export const scriptsDir = join(rootDir, "..");
+export const scriptsDir = join(rootDir, "scripts");
 
 export const packagesDir = join(rootDir, "packages");
 
